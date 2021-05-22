66f3dffabcf8ad2eb24ae980acc200130cd3f905
Takler <73590542+Takler@users.noreply.github.com>
2021-05-22 22:11:34 +0200

    PatientService filled with the basic CRUD logic

--- src/main/java/hu/demo/vaccination/service/PatientService.java
+++ src/main/java/hu/demo/vaccination/service/PatientService.java
@@ -1,4 +1,41 @@
 package hu.demo.vaccination.service;
 
+import hu.demo.vaccination.domain.Patient;
+import hu.demo.vaccination.dto.patient.PatientCreateData;
+import hu.demo.vaccination.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class PatientService {
+    private final PatientRepository patientRepository;
+
+    @Autowired
+    public PatientService(PatientRepository patientRepository) {
+        this.patientRepository = patientRepository;
+    }
+
+    public List<Patient> getPatients() {
+        return patientRepository.getPatients();
+    }
+
+    public Patient getPatient(int id) {
+        return patientRepository.getPatient(id);
+    }
+
+    public boolean createPatient(PatientCreateData data) {
+        return patientRepository.createPatient(data);
+    }
+
+    public boolean updatePatient(int id, PatientCreateData data) {
+        return patientRepository.updatePatient(id, data);
+    }
+
+    public boolean deletePatient(int id) {
+        return patientRepository.deletePatient(id);
+    }
 }
