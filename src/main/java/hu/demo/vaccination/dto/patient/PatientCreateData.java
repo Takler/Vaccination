183ef32b21ed49b0d91ed68c17d012a08400fa7c
Takler <73590542+Takler@users.noreply.github.com>
2021-05-22 21:08:56 +0200

    Patient dto created

--- src/main/java/hu/demo/vaccination/dto/patient/PatientCreateData.java
+++ src/main/java/hu/demo/vaccination/dto/patient/PatientCreateData.java
@@ -1,4 +1,115 @@
 package hu.demo.vaccination.dto.patient;
 
+import java.util.Date;
+
 public class PatientCreateData {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String mothersName;
+    private String gender;
+    private Date dateOfBirth;
+    private String email;
+    private String city;
+    private String zipCode;
+    private String address;
+    private String telephoneNumber;
+    private boolean underlyingMedicalCondition;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMothersName() {
+        return mothersName;
+    }
+
+    public void setMothersName(String mothersName) {
+        this.mothersName = mothersName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public boolean isUnderlyingMedicalCondition() {
+        return underlyingMedicalCondition;
+    }
+
+    public void setUnderlyingMedicalCondition(boolean underlyingMedicalCondition) {
+        this.underlyingMedicalCondition = underlyingMedicalCondition;
+    }
 }
