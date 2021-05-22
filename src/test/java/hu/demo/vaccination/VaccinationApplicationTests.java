70510a47523ebd7798bffcb0e9801c819059b8b1
Takler <73590542+Takler@users.noreply.github.com>
2021-05-22 14:09:06 +0200

    Initial commit

---
+++ src/test/java/hu/demo/vaccination/VaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.demo.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
