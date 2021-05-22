a6774db4bb299f6985502139656e4134c912b83e
Takler <73590542+Takler@users.noreply.github.com>
2021-05-22 22:19:32 +0200

    PatientController created with the basic CRUD mappings

--- src/main/java/hu/demo/vaccination/controller/PatientController.java
+++ src/main/java/hu/demo/vaccination/controller/PatientController.java
@@ -1,4 +1,69 @@
 package hu.demo.vaccination.controller;
 
+import hu.demo.vaccination.domain.Patient;
+import hu.demo.vaccination.dto.patient.PatientCreateData;
+import hu.demo.vaccination.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/patient")
 public class PatientController {
+
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Patient>> getPatients() {
+        List<Patient> patients = patientService.getPatients();
+        return new ResponseEntity<>(patients, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatient(@PathVariable int id) {
+        Patient patient = patientService.getPatient(id);
+        if (patient != null) {
+            return new ResponseEntity<>(patient, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createPatient(@RequestBody PatientCreateData data) {
+        boolean saveSuccessful = patientService.createPatient(data);
+        if (saveSuccessful) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updatePatient(@PathVariable int id, @RequestBody PatientCreateData data) {
+        boolean updateSuccessful = patientService.updatePatient(id, data);
+        if (updateSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePatient(@PathVariable int id) {
+        boolean deleteSuccessful = patientService.deletePatient(id);
+        if (deleteSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
