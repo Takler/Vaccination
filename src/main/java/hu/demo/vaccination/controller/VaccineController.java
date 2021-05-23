0f19fdebfb59092531cab8772a689e6a0a31b702
Kristóf Nagybán <kristof.nagyban@gmail.com>
2021-05-23 23:04:41 +0200

    VaccineController created.

--- src/main/java/hu/demo/vaccination/controller/VaccineController.java
+++ src/main/java/hu/demo/vaccination/controller/VaccineController.java
@@ -1,11 +1,69 @@
 package hu.demo.vaccination.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import hu.demo.vaccination.domain.Vaccine;
+import hu.demo.vaccination.dto.VaccineCreateData;
+import hu.demo.vaccination.service.VaccineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/vaccine")
 public class VaccineController {
 
+    private final VaccineService vaccineService;
 
+    @Autowired
+    public VaccineController(VaccineService vaccineService) {
+        this.vaccineService = vaccineService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Vaccine>> getVaccines() {
+        List<Vaccine> vaccines = vaccineService.getPatients();
+        return new ResponseEntity<>(vaccines, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Vaccine> getVaccine(@PathVariable int id) {
+        Vaccine vaccine = vaccineService.getVaccine(id);
+        if (vaccine != null) {
+            return new ResponseEntity<>(vaccine, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createVaccine(@RequestBody VaccineCreateData data) {
+        boolean saveSuccessful = vaccineService.createVaccine(data);
+        if (saveSuccessful) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateVaccine(@PathVariable int id, @RequestBody VaccineCreateData data) {
+        boolean updateSuccessful = vaccineService.updatePatient(id, data);
+        if (updateSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteVaccine(@PathVariable int id) {
+        boolean deleteSuccessful = vaccineService.deleteVaccine(id);
+        if (deleteSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/hu/demo/vaccination/service/VaccineService.java
+++ src/main/java/hu/demo/vaccination/service/VaccineService.java
@@ -1,9 +1,32 @@
 package hu.demo.vaccination.service;
 
+import hu.demo.vaccination.domain.Vaccine;
+import hu.demo.vaccination.dto.VaccineCreateData;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
 public class VaccineService {
+    public List<Vaccine> getPatients() {
+        return null;
+    }
+
+    public Vaccine getVaccine(int id) {
+        return null;
+    }
+
+    public boolean createVaccine(VaccineCreateData data) {
+        return false;
+    }
+
+    public boolean updatePatient(int id, VaccineCreateData data) {
+        return false;
+    }
+
+    public boolean deleteVaccine(int id) {
+        return false;
+    }
 }
