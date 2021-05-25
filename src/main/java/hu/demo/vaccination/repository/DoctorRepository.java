665150c14dd0a6db48a4b082d2ddd8502bf66007
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-26 00:49:36 +0200

    DoctorRepository createDoctor()

--- src/main/java/hu/demo/vaccination/repository/DoctorRepository.java
+++ src/main/java/hu/demo/vaccination/repository/DoctorRepository.java
@@ -1,8 +1,34 @@
 package hu.demo.vaccination.repository;
 
+import hu.demo.vaccination.dto.DoctorCreate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.SQLException;
 
 @Repository
 public class DoctorRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DoctorRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public int createDoctor(DoctorCreate doctorCreate) {
+        String sqlInsert = "INSERT INTO vaccination (id, firstName, lastName, email, address, telephone_nember, type, date_of_birth)" +
+                "VALUES (?,?,?,?,?,?,?,?)";
+        try {
+            int result = jdbcTemplate.update(sqlInsert, doctorCreate.getId(), doctorCreate.getFirstName(), doctorCreate.getLastName(),
+                    doctorCreate.getAddress(), doctorCreate.getTelephoneNumber(), doctorCreate.getType(),
+                    doctorCreate.getDate_of_birth());
+            return result;
+        } catch (DataAccessException exception) {
+            return -1;
+        }
+    }
 
 }
