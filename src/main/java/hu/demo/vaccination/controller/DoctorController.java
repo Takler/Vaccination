d017cab1c1219133f9e27a075f81e2904dd0c471
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-25 23:52:59 +0200

    DoctorController template

--- src/main/java/hu/demo/vaccination/controller/DoctorController.java
+++ src/main/java/hu/demo/vaccination/controller/DoctorController.java
@@ -1,22 +1,42 @@
 package hu.demo.vaccination.controller;
 
 import hu.demo.vaccination.dto.DoctorCreate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/doctor")
 public class DoctorController {
-    @GetMapping
-    public ResponseEntity<List<DoctorCreate>> getDoctor(){
 
-        return
+    @Autowired
+
+
+    @PostMapping
+    public ResponseEntity<Void> createDoctor(@RequestBody DoctorCreate doctorCreate) {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @GetMapping
+    public ResponseEntity<List<DoctorCreate>> getDoctorsList() {
+        return new ResponseEntity<>(List < DoctorCreate >, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorCreate> getDoctor(@PathVariable int id) {
+        return new ResponseEntity<>(DoctorCreate, HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateDoctor(@RequestBody DoctorCreate doctorCreate) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable int id) {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
