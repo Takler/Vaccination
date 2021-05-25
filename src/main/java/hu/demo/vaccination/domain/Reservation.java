13b05a6b76059553dd7bda5e3e3978a3825f4388
Takler <73590542+Takler@users.noreply.github.com>
2021-05-25 20:41:04 +0200

    Reservation related initialization

---
+++ src/main/java/hu/demo/vaccination/domain/Reservation.java
@@ -0,0 +1,41 @@
+package hu.demo.vaccination.domain;
+
+public class Reservation {
+
+    private int id;
+    private int patientId;
+    private int centerId;
+    private int vaccineId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getCenterId() {
+        return centerId;
+    }
+
+    public void setCenterId(int centerId) {
+        this.centerId = centerId;
+    }
+
+    public int getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(int vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+}
---
+++ src/main/java/hu/demo/vaccination/dto/ReservationCreateData.java
@@ -0,0 +1,32 @@
+package hu.demo.vaccination.dto;
+
+public class ReservationCreateData {
+
+    private int patientId;
+    private int centerId;
+    private int vaccineId;
+
+    public int getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(int patientId) {
+        this.patientId = patientId;
+    }
+
+    public int getCenterId() {
+        return centerId;
+    }
+
+    public void setCenterId(int centerId) {
+        this.centerId = centerId;
+    }
+
+    public int getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(int vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+}
