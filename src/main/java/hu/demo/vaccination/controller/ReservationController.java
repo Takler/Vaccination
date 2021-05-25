ba1e2b348e26578a91382c9592f43068d6500f5a
Takler <73590542+Takler@users.noreply.github.com>
2021-05-25 20:53:41 +0200

    ReservationController added

---
+++ src/main/java/hu/demo/vaccination/controller/ReservationController.java
@@ -0,0 +1,69 @@
+package hu.demo.vaccination.controller;
+
+import hu.demo.vaccination.domain.Reservation;
+import hu.demo.vaccination.dto.ReservationCreateData;
+import hu.demo.vaccination.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservation")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    @Autowired
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getReservations() {
+        List<Reservation> reservations = reservationService.getReservations();
+        return new ResponseEntity<>(reservations, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservation(@PathVariable int id) {
+        Reservation reservation = reservationService.getReservation(id);
+        if (reservation != null) {
+            return new ResponseEntity<>(reservation, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createReservation(@RequestBody ReservationCreateData data) {
+        boolean saveSuccessful = reservationService.createReservation(data);
+        if (saveSuccessful) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updateReservation(@PathVariable int id, @RequestBody ReservationCreateData data) {
+        boolean updateSuccessful = reservationService.updateReservation(id, data);
+        if (updateSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteReservation(@PathVariable int id) {
+        boolean deleteSuccessful = reservationService.deleteReservation(id);
+        if (deleteSuccessful) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
