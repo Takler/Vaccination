e521f98edb44cbc90e3ea5ae2a9f8661e950e7ad
paljoz <paljoz@vipmail.hu>
2021-05-25 12:56:41 +0200

    create CenterCreateData

---
+++ src/main/java/hu/demo/vaccination/dto/CenterCreateData.java
@@ -0,0 +1,49 @@
+package hu.demo.vaccination.dto;
+
+public class CenterCreateData {
+    private String name;
+    private String city;
+    private String email;
+    private String telephoneNumber;
+    private int dailyCapacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public int getDailyCapacity() {
+        return dailyCapacity;
+    }
+
+    public void setDailyCapacity(int dailyCapacity) {
+        this.dailyCapacity = dailyCapacity;
+    }
+}
