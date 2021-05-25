5b0cf84c82295784e8cdcc4de052e6cec527afde
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-25 14:49:44 +0200

    start

---
+++ src/main/java/hu/demo/vaccination/controller/DoctorController.java
@@ -0,0 +1,15 @@
+package hu.demo.vaccination.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/doctor")
+public class DoctorController {
+
+}
---
+++ src/main/java/hu/demo/vaccination/repository/DoctorRepository.java
@@ -0,0 +1,8 @@
+package hu.demo.vaccination.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DoctorRepository {
+
+}
