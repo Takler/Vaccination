cc9010b1668aa73e7a8c3b331f4f7ee8d98dfebe
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-27 10:24:43 +0200

    InitializationRepository modify: Doctor and Shift static String names

--- src/main/java/hu/demo/vaccination/repository/InitializationRepository.java
+++ src/main/java/hu/demo/vaccination/repository/InitializationRepository.java
@@ -18,22 +18,22 @@ public class InitializationRepository {
     public void initialization() {
         //TODO remove comment when ready :)
 
-        //jdbc.execute(ShiftInit.SHIFT_DROP_TABLE);
+        jdbc.execute(ShiftInit.CREATE_TABLE_SHIFT);
         jdbc.execute(InventoryInit.INVENTORY_DROP_TABLE);
         jdbc.execute(VaccinationInit.VACCINATION_DROP_TABLE);
         jdbc.execute(ReservationInit.RESERVATION_DROP_TABLE);
 
-        jdbc.execute(DoctorInit.DOCTOR_DROP_TABLE);
+        jdbc.execute(DoctorInit.DROP_TABLE_DOCTOR);
         jdbc.execute(PatientInit.PATIENT_DROP_TABLE);
         jdbc.execute(VaccineInit.VACCINE_DROP_TABLE);
         jdbc.execute(CenterInit.CENTER_DROP_TABLE);
 
-        jdbc.execute(DoctorInit.DOCTOR_CREATE_TABLE);
+        jdbc.execute(DoctorInit.CREATE_TABLE_DOCTOR);
         jdbc.execute(PatientInit.PATIENT_INIT_TABLE);
         jdbc.execute(VaccineInit.VACCINE_INIT_TABLE);
         jdbc.execute(CenterInit.CENTER_INIT_TABLE);
 
-        jdbc.execute(DoctorInit.DOCTOR_SAMPLE_DATA_INSERT);
+        jdbc.execute(DoctorInit.INSERT_SAMLE_DATA_DOCTOR);
         jdbc.execute(VaccineInit.VACCINE_DATA_INSERT);
         jdbc.execute(PatientInit.PATIENT_SAMPLE_DATA_INSERT);
         jdbc.execute(CenterInit.CENTER_SAMPLE_DATA_INSERT);
