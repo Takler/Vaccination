7ca580dc4db999b1130750e7cdb8cb9843b7eef0
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-27 18:52:25 +0200

    Modified HasLastName: modifid method name - camelcase

--- src/main/java/hu/demo/vaccination/service/HasLastName.java
+++ src/main/java/hu/demo/vaccination/service/HasLastName.java
@@ -3,5 +3,5 @@ package hu.demo.vaccination.service;
 import java.util.List;
 
 public interface HasLastName extends Requestable{
-    public List<String> getLastname(String firstName);
+    public List<String> getLastName(String firstName);
 }
