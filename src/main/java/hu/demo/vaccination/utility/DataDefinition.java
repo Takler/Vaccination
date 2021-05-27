7d88f34db467332f931e245072a3cbeef6e78d1a
Kende Szabolcs <kende.szabolcs@gmail.com>
2021-05-27 16:05:13 +0200

    Corrected DOCTOR_CREATE_TABLE: deleted "type" field, line break Corrected DOCTOR_INSER_SAMPLE_DATA: deleted "type" filed values

--- src/main/java/hu/demo/vaccination/utility/DataDefinition.java
+++ src/main/java/hu/demo/vaccination/utility/DataDefinition.java
@@ -11,9 +11,14 @@ public enum DataDefinition {
     VACCINE_DROP_TABLE("DROP TABLE IF EXISTS vaccine"),
     CENTER_DROP_TABLE("DROP TABLE IF EXISTS center"),
 
-    DOCTOR_CREATE_TABLE("CREATE TABLE IF NOT EXISTS doctor (id int PRIMARY KEY AUTO_INCREMENT, " +
-            "first_name varchar(20) NOT NULL, last_name varchar(20) NOT NULL, email varchar(60), address varchar(200), " +
-            "telephone_number varchar(15), type varchar(10), date_of_birth datetime)"),
+    DOCTOR_CREATE_TABLE("CREATE TABLE IF NOT EXISTS doctor " +
+            "(id int PRIMARY KEY AUTO_INCREMENT, " +
+            "first_name varchar(20) NOT NULL, " +
+            "last_name varchar(20) NOT NULL, " +
+            "email varchar(60), " +
+            "address varchar(200), " +
+            "telephone_number varchar(15), " +
+            "date_of_birth datetime)"),
     PATIENT_CREATE_TABLE("CREATE TABLE patient " +
             "(id INT PRIMARY KEY, " +
             "first_name VARCHAR(250) NOT NULL, " +
@@ -54,10 +59,10 @@ public enum DataDefinition {
 
     DOCTOR_INSERT_SAMPLE_DATA("INSERT INTO doctor (first_name, last_name, email, address, telephone_number, " +
             "type, date_of_birth) VALUES " +
-            "('Gipsz',  'Jakab',  'jakab.gipsz@gmail.com',   '1111, Budapest Gipsz utca 1',  '+36 11 111-1111', 'prof.', '1960.10.10'), " +
-            "('Kerek',  'Béla',   'bela.kerek@gmail.com',    '2222, Budapest Kerek utca 2',  '+36 22 222-2222', 'adj.',  '1970.10.10'), " +
-            "('Karcsú', 'Virág',  'virag.karcsu@gmail.com',  '3333, Budapest Karcsú utca 3', '+36 33 333-3333', 'ápóló', '1980.10.10'), " +
-            "('Gipsz',  'Andrea', 'andrea.gipsz@gmail.com',  '4444, Budapest Gipsz utca 4',  '+36 44 444-4444', 'ápóló', '1990.10.10')"),
+            "('Gipsz',  'Jakab',  'jakab.gipsz@gmail.com',   '1111, Budapest Gipsz utca 1',  '+36 11 111-1111', '1960.10.10'), " +
+            "('Kerek',  'Béla',   'bela.kerek@gmail.com',    '2222, Budapest Kerek utca 2',  '+36 22 222-2222',  '1970.10.10'), " +
+            "('Karcsú', 'Virág',  'virag.karcsu@gmail.com',  '3333, Budapest Karcsú utca 3', '+36 33 333-3333', '1980.10.10'), " +
+            "('Gipsz',  'Andrea', 'andrea.gipsz@gmail.com',  '4444, Budapest Gipsz utca 4',  '+36 44 444-4444', '1990.10.10')"),
     VACCINE_INSERT_SAMPLE_DATA("INSERT INTO vaccine " +
             "(name, type, storage_temperature, age_limit_min, age_limit_max, shots_needed, next_shot_id, " +
             "days_until_next_shot, fully_vaccinated_time_period, applicable, applicable_for_pregnant, " +
