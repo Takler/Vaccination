56a5865665975ca385425a6d45dab131fc75358f
paljoz <paljoz@vipmail.hu>
2021-05-27 15:26:24 +0200

    update InventoryCreateData: added getters/setters

--- src/main/java/hu/demo/vaccination/dto/InventoryCreateData.java
+++ src/main/java/hu/demo/vaccination/dto/InventoryCreateData.java
@@ -5,4 +5,36 @@ public class InventoryCreateData {
     private int centerId;
     private int vaccineId;
     private int amount;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCenterId() {
+        return centerId;
+    }
+
+    public void setCenterId(int centerId) {
+        this.centerId = centerId;
+    }
+
+    public int getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(int vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
