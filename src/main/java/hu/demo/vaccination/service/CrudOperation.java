295be0c836995a259a71e1bf23bd37f42cea527e
Takler <73590542+Takler@users.noreply.github.com>
2021-05-28 16:37:56 +0200

    added more methods for the CrudOperation interface (team code review)

--- src/main/java/hu/demo/vaccination/service/CrudOperation.java
+++ src/main/java/hu/demo/vaccination/service/CrudOperation.java
@@ -3,5 +3,13 @@ package hu.demo.vaccination.service;
 import java.util.List;
 
 public interface CrudOperation<T, U> {
-    public T getById(int id);
+    T getById(int id);
+
+    List<T> findAll();
+
+    boolean save(U createData);
+
+    boolean update(int id, U createData);
+
+    boolean delete(int id);
 }
