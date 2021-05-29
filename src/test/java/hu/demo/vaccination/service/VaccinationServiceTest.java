5ed113b2ab7458b37627866873dfe5443e3e2bee
Kristóf Nagybán <kristof.nagyban@gmail.com>
2021-05-29 16:29:27 +0200

    VaccinationServiceTest vaccinations list test data initialization.

--- src/test/java/hu/demo/vaccination/service/VaccinationServiceTest.java
+++ src/test/java/hu/demo/vaccination/service/VaccinationServiceTest.java
@@ -213,6 +213,32 @@ class VaccinationServiceTest {
         patient21.setUnderlyingMedicalCondition(true);
         patients.add(patient21);
 
+        vaccinations = new ArrayList<>();
+        vaccinations.add(new Vaccination(1, 1, 3, 1, LocalDate.now().minusDays(10), false));
+        vaccinations.add(new Vaccination(2, 1, 4, 2, LocalDate.now().minusDays(9), false));
+        vaccinations.add(new Vaccination(3, 1, 5, 3, LocalDate.now().minusDays(8), false));
+        vaccinations.add(new Vaccination(4, 3, 6, 4, LocalDate.now().minusDays(7), false));
+        vaccinations.add(new Vaccination(5, 4, 7, 5, LocalDate.now().minusDays(6), false));
+        vaccinations.add(new Vaccination(6, 2, 8, 6, LocalDate.now().minusDays(5), false));
+        vaccinations.add(new Vaccination(7, 2, 9, 1, LocalDate.now().minusDays(10), false));
+        vaccinations.add(new Vaccination(8, 1, 10, 2, LocalDate.now().minusDays(9), false));
+        vaccinations.add(new Vaccination(9, 2, 11, 2, LocalDate.now().minusDays(9), false));
+        vaccinations.add(new Vaccination(10, 3, 12, 4, LocalDate.now().minusDays(7), false));
+        vaccinations.add(new Vaccination(11, 3, 14, 5, LocalDate.now().minusDays(6), false));
+        vaccinations.add(new Vaccination(12, 6, 15, 1, LocalDate.now().minusDays(10), false));
+        vaccinations.add(new Vaccination(13, 1, 17, 2, LocalDate.now().minusDays(9), false));
+        vaccinations.add(new Vaccination(14, 6, 18, 3, LocalDate.now().minusDays(8), false));
+        vaccinations.add(new Vaccination(15, 7, 20, 5, LocalDate.now().minusDays(6), false));
+        vaccinations.add(new Vaccination(16, 2, 21, 5, LocalDate.now().minusDays(6), false));
+
+        vaccinations.add(new Vaccination(17, 1, 4, 6, LocalDate.now().minusDays(4), false));
+        vaccinations.add(new Vaccination(18, 3, 6, 6, LocalDate.now().minusDays(4), false));
+        vaccinations.add(new Vaccination(19, 5, 7, 7, LocalDate.now().minusDays(3), false));
+        vaccinations.add(new Vaccination(20, 3, 14, 7, LocalDate.now().minusDays(3), false));
+        vaccinations.add(new Vaccination(21, 1, 17, 8, LocalDate.now().minusDays(2), false));
+        vaccinations.add(new Vaccination(22, 6, 18, 8, LocalDate.now().minusDays(2), false));
+        vaccinations.add(new Vaccination(23, 2, 21, 8, LocalDate.now().minusDays(2), false));
+
     }
 
 
