165514e4aef1688f300e9e9cb0b42827ef4999c5
Takler <73590542+Takler@users.noreply.github.com>
2021-06-02 15:42:12 +0200

    Dto created for file input path (Team Code Review)

---
+++ src/main/java/hu/demo/vaccination/dto/InputCreateData.java
@@ -0,0 +1,14 @@
+package hu.demo.vaccination.dto;
+
+public class InputCreateData {
+
+    private String input;
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
