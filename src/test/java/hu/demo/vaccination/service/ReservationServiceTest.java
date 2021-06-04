b3f87caf08fb7207343dde10a0ee11988439004c
Takler <73590542+Takler@users.noreply.github.com>
2021-06-04 19:18:28 +0200

    added more tests for ReservationService

--- src/test/java/hu/demo/vaccination/service/ReservationServiceTest.java
+++ src/test/java/hu/demo/vaccination/service/ReservationServiceTest.java
@@ -2,6 +2,7 @@ package hu.demo.vaccination.service;
 
 import hu.demo.vaccination.domain.Reservation;
 import hu.demo.vaccination.dto.reservation.PatientReservationData;
+import hu.demo.vaccination.dto.reservation.ReservationCreateData;
 import hu.demo.vaccination.repository.ReservationRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,12 +11,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ReservationServiceTest {
+
+    private static final int PATIENT_ID = 123123123;
+    private static final int OTHER_ID = 1;
 
     private ReservationService reservationService;
 
@@ -35,42 +41,110 @@ class ReservationServiceTest {
 
     @Test
     void test_getPatientReservation_receiveCorrectData() {
-        int patientId = 123123123;
-        int otherId = 1;
         String centerName = "Honvéd Kórház";
         String vaccineName = "Pzizer";
         String patientName = "Test Elek";
 
-        Reservation originalReservation = new Reservation();
-        originalReservation.setId(otherId);
-        originalReservation.setCenterId(otherId);
-        originalReservation.setVaccineId(otherId);
-        originalReservation.setPatientId(patientId);
-        originalReservation.setRegistration(LocalDate.now());
-        originalReservation.setNextShot(LocalDate.now().plusDays(14));
+        Reservation originalReservation = getSampleReservation();
 
         PatientReservationData originalPatientReservationData = new PatientReservationData();
-        originalPatientReservationData.setReservationId(otherId);
+        originalPatientReservationData.setReservationId(OTHER_ID);
         originalPatientReservationData.setRegistration(originalReservation.getRegistration());
         originalPatientReservationData.setNextShot(originalPatientReservationData.getNextShot());
 
-        when(reservationRepositoryMock.getById(otherId)).thenReturn(originalReservation);
-        when(reservationRepositoryMock.getPatientReservation(patientId)).thenReturn(originalPatientReservationData);
-        when(centerServiceMock.getName(otherId)).thenReturn(centerName);
-        when(vaccineServiceMock.getName(otherId)).thenReturn(vaccineName);
-        when(patientServiceMock.getName(patientId)).thenReturn(patientName);
+        when(reservationRepositoryMock.getById(OTHER_ID)).thenReturn(originalReservation);
+        when(reservationRepositoryMock.getPatientReservation(PATIENT_ID)).thenReturn(originalPatientReservationData);
+        when(centerServiceMock.getName(OTHER_ID)).thenReturn(centerName);
+        when(vaccineServiceMock.getName(OTHER_ID)).thenReturn(vaccineName);
+        when(patientServiceMock.getName(PATIENT_ID)).thenReturn(patientName);
 
-        PatientReservationData resultData = reservationService.getPatientReservation(patientId);
+        PatientReservationData resultData = reservationService.getPatientReservation(PATIENT_ID);
 
         assertEquals(centerName, resultData.getCenterName());
         assertEquals(vaccineName, resultData.getVaccineName());
         assertEquals(patientName, resultData.getPatientName());
-        verify(reservationRepositoryMock, times(1)).getById(otherId);
-        verify(centerServiceMock, times(1)).getName(otherId);
-        verify(vaccineServiceMock, times(1)).getName(otherId);
-        verify(patientServiceMock, times(1)).getName(patientId);
+        verify(reservationRepositoryMock, times(1)).getById(OTHER_ID);
+        verify(centerServiceMock, times(1)).getName(OTHER_ID);
+        verify(vaccineServiceMock, times(1)).getName(OTHER_ID);
+        verify(patientServiceMock, times(1)).getName(PATIENT_ID);
 
     }
 
+    @Test
+    void test_findAll_receiveAllReservations() {
+        Reservation testReservation = getSampleReservation();
+
+        when(reservationRepositoryMock.findAll()).thenReturn(List.of(testReservation));
+
+        List<Reservation> resultReservations = reservationService.findAll();
+
+        assertEquals(1, resultReservations.size());
+        assertEquals(testReservation, resultReservations.get(0));
+        verify(reservationRepositoryMock, times(1)).findAll();
+
+    }
+
+    @Test
+    void test_getById_receiveOneReservation() {
+        Reservation testReservation = getSampleReservation();
+        int id = testReservation.getId();
+
+        when(reservationRepositoryMock.getById(id)).thenReturn(testReservation);
+
+        Reservation resultReservation = reservationService.getById(id);
+
+        assertEquals(testReservation, resultReservation);
+        verify(reservationRepositoryMock, times(1)).getById(id);
+
+    }
+
+    @Test
+    void test_save_onceCalled() {
+        ReservationCreateData testData = getSampleReservationCreateData();
+
+        when(reservationRepositoryMock.save(testData)).thenReturn(true);
+
+        assertTrue(reservationService.save(testData));
+        verify(reservationRepositoryMock, times(1)).save(testData);
+    }
+
+    @Test
+    void test_update_onceCalled() {
+        ReservationCreateData testData = getSampleReservationCreateData();
+
+        when(reservationRepositoryMock.update(OTHER_ID, testData)).thenReturn(true);
+
+        assertTrue(reservationService.update(OTHER_ID, testData));
+        verify(reservationRepositoryMock, times(1)).update(OTHER_ID, testData);
+    }
+
+    @Test
+    void test_delete_onceCalled() {
+        when(reservationRepositoryMock.delete(OTHER_ID)).thenReturn(true);
+
+        assertTrue(reservationService.delete(OTHER_ID));
+        verify(reservationRepositoryMock, times(1)).delete(OTHER_ID);
+    }
+
+    private Reservation getSampleReservation() {
+        Reservation reservation = new Reservation();
+        reservation.setId(OTHER_ID);
+        reservation.setCenterId(OTHER_ID);
+        reservation.setVaccineId(OTHER_ID);
+        reservation.setPatientId(PATIENT_ID);
+        reservation.setRegistration(LocalDate.now());
+        reservation.setNextShot(LocalDate.now().plusDays(14));
+        return reservation;
+    }
+
+    private ReservationCreateData getSampleReservationCreateData() {
+        ReservationCreateData data = new ReservationCreateData();
+        data.setCenterId(OTHER_ID);
+        data.setVaccineId(OTHER_ID);
+        data.setPatientId(PATIENT_ID);
+        data.setRegistration(LocalDate.now());
+        data.setNextShot(LocalDate.now().plusDays(14));
+        return data;
+    }
 
 }
