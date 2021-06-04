56edb0ec0a7dff9ca165fb79fe64262f36300e84
Takler <73590542+Takler@users.noreply.github.com>
2021-06-04 12:03:35 +0200

    Team Code Review: added FilePathDefinition

---
+++ src/main/java/hu/demo/vaccination/utility/FilePathDefinition.java
@@ -0,0 +1,11 @@
+package hu.demo.vaccination.utility;
+
+public enum FilePathDefinition {
+    SAVE_PATH("backupfiles");
+
+    private final String definition;
+
+    FilePathDefinition(String definition) {
+        this.definition = definition;
+    }
+}
