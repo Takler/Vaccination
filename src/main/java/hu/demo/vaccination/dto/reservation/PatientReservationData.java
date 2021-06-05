58a47dc14c97f246139d957db8e07e60cd90d537
Takler <73590542+Takler@users.noreply.github.com>
2021-06-05 22:22:58 +0200

    added more constructors for easier testing

--- src/main/java/hu/demo/vaccination/dto/reservation/PatientReservationData.java
+++ src/main/java/hu/demo/vaccination/dto/reservation/PatientReservationData.java
@@ -15,4 +15,16 @@ public class PatientReservationData {
     private LocalDate registration;
     private LocalDate nextShot;
 
+    public PatientReservationData() {
+    }
+
+    public PatientReservationData(int reservationId, int patientId, String patientName, String centerName, String vaccineName, LocalDate registration, LocalDate nextShot) {
+        this.reservationId = reservationId;
+        this.patientId = patientId;
+        this.patientName = patientName;
+        this.centerName = centerName;
+        this.vaccineName = vaccineName;
+        this.registration = registration;
+        this.nextShot = nextShot;
+    }
 }
