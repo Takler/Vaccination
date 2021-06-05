52addc10f804bdecb424276f9e7e58754ad4ce78
paljoz <paljoz@vipmail.hu>
2021-06-05 20:49:46 +0200

    refactor: dto classes now have the @Data annotation to generate simple POJO boilerplate

--- src/main/java/hu/demo/vaccination/dto/InventoryCreateData.java
+++ src/main/java/hu/demo/vaccination/dto/InventoryCreateData.java
@@ -1,31 +1,11 @@
 package hu.demo.vaccination.dto;
 
+import lombok.Data;
+
+@Data
 public class InventoryCreateData {
     private int centerId;
     private int vaccineId;
     private int amount;
 
-    public int getCenterId() {
-        return centerId;
-    }
-
-    public void setCenterId(int centerId) {
-        this.centerId = centerId;
-    }
-
-    public int getVaccineId() {
-        return vaccineId;
-    }
-
-    public void setVaccineId(int vaccineId) {
-        this.vaccineId = vaccineId;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
 }
--- src/main/java/hu/demo/vaccination/dto/inventory/InventoryStockData.java
+++ src/main/java/hu/demo/vaccination/dto/inventory/InventoryStockData.java
@@ -1,22 +1,10 @@
 package hu.demo.vaccination.dto.inventory;
 
+import lombok.Data;
+
+@Data
 public class InventoryStockData {
     private String vaccineName;
     private int amount;
 
-    public String getVaccineName() {
-        return vaccineName;
-    }
-
-    public void setVaccineName(String vaccineName) {
-        this.vaccineName = vaccineName;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    public void setAmount(int amount) {
-        this.amount = amount;
-    }
 }
