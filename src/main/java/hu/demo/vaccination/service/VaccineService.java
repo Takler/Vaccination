c7a8aef9d67bef7e4a6d9f509bf5a4bcc0e17fed
Kristóf Nagybán <kristof.nagyban@gmail.com>
2021-06-05 20:38:10 +0200

    VaccinationService VaccineService circular dependency problem solved.

--- src/main/java/hu/demo/vaccination/service/VaccineService.java
+++ src/main/java/hu/demo/vaccination/service/VaccineService.java
@@ -1,23 +1,33 @@
 package hu.demo.vaccination.service;
 
+import hu.demo.vaccination.domain.Patient;
 import hu.demo.vaccination.domain.Vaccine;
 import hu.demo.vaccination.dto.VaccineCreateData;
 import hu.demo.vaccination.repository.VaccineRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
 public class VaccineService implements CrudOperation<Vaccine, VaccineCreateData>, Nameable {
 
     private final VaccineRepository vaccineRepository;
+    private final VaccinationService vaccinationService;
 
     @Autowired
-    public VaccineService(VaccineRepository vaccineRepository) {
+    public VaccineService(VaccineRepository vaccineRepository, @Lazy VaccinationService vaccinationService) {
         this.vaccineRepository = vaccineRepository;
+        this.vaccinationService = vaccinationService;
+    }
+
+    public List<Vaccine> getVaccineForPatient(Patient patient) {
+        return null;
     }
 
     @Override
@@ -48,5 +58,19 @@ public class VaccineService implements CrudOperation<Vaccine, VaccineCreateData>
     @Override
     public String getName(int id) {
         return vaccineRepository.getVaccine(id).getName();
+    }
+
+    private List<Vaccine> getAllApplicableVaccineForPatient(Patient patient) {
+        List<Vaccine> vaccines = findAll().stream()
+                .filter(Vaccine::isApplicable)
+                .collect(Collectors.toList());
+        vaccines = vaccines.stream()
+                .filter(vaccine ->
+                        LocalDate.now().minusYears(vaccine.getAgeLimitMin()).isBefore(patient.getDateOfBirth()) &&
+                                LocalDate.now().minusYears(vaccine.getAgeLimitMax()).isAfter(patient.getDateOfBirth()))
+                .filter(vaccine -> !patient.isPregnant() || vaccine.isApplicableForPregnant())
+                .filter(vaccine -> !patient.isUnderlyingMedicalCondition() || vaccine.isApplicableForChronic())
+                .collect(Collectors.toList());
+        return vaccines;
     }
 }
