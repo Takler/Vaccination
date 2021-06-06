cfe3f5e8c2b67849d28a8f559a7a84b90ad6d081
paljoz <paljoz@vipmail.hu>
2021-06-06 22:53:02 +0200

    fileSave implemented

--- src/main/java/hu/demo/vaccination/service/CenterService.java
+++ src/main/java/hu/demo/vaccination/service/CenterService.java
@@ -1,19 +1,27 @@
 package hu.demo.vaccination.service;
 
 import hu.demo.vaccination.domain.Center;
+import hu.demo.vaccination.dto.InputCreateData;
 import hu.demo.vaccination.dto.center.CenterCreateData;
 import hu.demo.vaccination.repository.CenterRepository;
 import hu.demo.vaccination.service.interfaces.CrudOperation;
+import hu.demo.vaccination.service.interfaces.FileHandler;
 import hu.demo.vaccination.service.interfaces.Nameable;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
-public class CenterService implements CrudOperation<Center, CenterCreateData>, Nameable {
+public class CenterService implements CrudOperation<Center, CenterCreateData>, Nameable, FileHandler {
     private final CenterRepository centerRepository;
 
     @Autowired
@@ -49,5 +57,30 @@ public class CenterService implements CrudOperation<Center, CenterCreateData>, N
     @Override
     public boolean delete(int id) {
         return centerRepository.deleteCenter(id);
+    }
+
+    @Override
+    public boolean fileSave(InputCreateData input) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(Files.newBufferedWriter(Paths.get(input.getInput())))) {
+            List<Center> centers = centerRepository.getCenters();
+            for (Center center : centers) {
+                bufferedWriter.write(convertToCsv(center));
+                bufferedWriter.newLine();
+            }
+            return true;
+        } catch (IOException e) {
+            log.error("save failed: " + e.getMessage());
+            return false;
+        }
+    }
+
+    @Override
+    public boolean fileLoad(InputCreateData input) {
+        // TODO
+        return false;
+    }
+
+    private String convertToCsv(Center center) {
+        return center.getId() + ";" + center.getName() + ";" + center.getCity() + ";" + center.getEmail() + ";" + center.getTelephoneNumber() + ";" + center.getDailyCapacity();
     }
 }
