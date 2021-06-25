ad71c1519065e2af21696ba7c4b1cd635c18a0c4
Takler <73590542+Takler@users.noreply.github.com>
2021-06-25 22:01:35 +0200

    test commit after release

--- src/main/java/hu/demo/vaccination/controller/PatientController.java
+++ src/main/java/hu/demo/vaccination/controller/PatientController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/patient")
+@RequestMapping("/api/patient") //test
 public class PatientController {
 
     private final PatientService patientService;
